6b4be4ddec67e0835865048e0313dffb249df524
Daniel Ponce <d_ponce718@yahoo.com>
2015-09-16 15:43:10 -0400

    added users to active admin, and a form to take payments using form craft

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'static_pages#home'
+  get    'login'             => 'devise/sessions#new'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
