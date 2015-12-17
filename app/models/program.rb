ae00876121d236e9441110753ad3e3e011c19de4
Daniel Ponce <d_ponce718@yahoo.com>
2015-12-16 20:04:56 -0500

    deleted ice cube

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,13 +1,6 @@
 class Program < ActiveRecord::Base
 	belongs_to :user
-	 include IceCube 
- 
-
-	 def schedul
-schedule = IceCube::Schedule.new(now = Time.zone.now) do |s|
-  s.add_recurrence_rule IceCube::Rule.monthly(2).day_of_week(1 => [1], 2 => [-1])
-end
-end
+	
 
  
 end
