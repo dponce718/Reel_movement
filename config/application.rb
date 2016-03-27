c84369738ec2c6815a6d9a4325e9b97903ce424e
Daniel Ponce <d_ponce718@yahoo.com>
2016-03-27 14:25:18 -0400

    added font assets gem

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,10 @@ module ReelMovement
     config.autoload_paths += Dir["#{Rails.root}/app/services"]
     config.time_zone = 'Eastern Time (US & Canada)'
     config.beginning_of_week = :monday
+
+    config.font_assets.origin = '*'
+
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
