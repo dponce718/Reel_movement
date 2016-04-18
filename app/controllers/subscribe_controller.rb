e3f84ba5fc8413e83b692c696f64593e2909fd82
Daniel Ponce <d_ponce718@yahoo.com>
2016-04-18 16:15:31 -0400

    added stripe id to webhooks

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -5,8 +5,11 @@ class SubscribeController < ApplicationController
 # reference https://stripe.com/docs/webhooks
 def webhook
   
+  StripeEvent.event_retriever = lambda do |params|
+  return nil if StripeWebhook.exists?(stripe_id: params[:id])
   StripeWebhook.create!(stripe_id: params[:id])
   Stripe::Event.retrieve(params[:id])
+end
 
   begin
     event_json = JSON.parse(request.body.read)
