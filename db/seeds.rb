f06658847166a5120ee073ca6824d8936f66205d
Daniel Ponce <d_ponce718@yahoo.com>
2016-04-22 09:40:43 -0400

    fixed programs

--- db/seeds.rb
+++ db/seeds.rb
@@ -144,7 +144,9 @@ schedule: '<div class="table-responsive">
 ')
 
  Program.create(title: "Core Conditioning", level: "level 1", frame: "http://api.vzaar.com/videos/5248691.embed", 
-code_one: '<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player center-block" frameborder="0" height="432" id="vzvd-5277901" mozallowfullscreen name="vzvd-5277901" src="//view.vzaar.com/5277901/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="768"></iframe>',
+code_one: '<iframe id="vzvd-6871452" name="vzvd-6871452" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6871452/player"></iframe>',
+code_two: '<iframe id="vzvd-6871465" name="vzvd-6871465" title="vzaar video player" class="vzaar-video-player" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6871465/player"></iframe>',
+code_three: '', 
 description:'The core is where you body’s center of gravity is located and is where all movement begins. Level 1 Core Conditioning gets your body up to speed by increasing core strength and core muscle endurance.In order to progress into more difficult programs having a strong core is mandatory.',
 style: 'core-conditioning',
 schedule: '<div class="table-responsive">
@@ -271,7 +273,10 @@ schedule: '<div class="table-responsive">
 )
 
 Program.create(title: "Cardiovascular Training", level: "level 1", frame: "http://api.vzaar.com/videos/5248691.embed", 
-code_one: '<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player center-block" frameborder="0" height="432" id="vzvd-5277940" mozallowfullscreen name="vzvd-5277940" src="//view.vzaar.com/5277940/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="768"></iframe>',
+code_one: '<iframe id="vzvd-6870658" name="vzvd-6870658" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6870658/player"></iframe>',
+code_two: '<iframe id="vzvd-6870681" name="vzvd-6870681" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6870681/player"></iframe>',
+code_three: '<iframe id="vzvd-6870708" name="vzvd-6870708" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6870708/player"></iframe>' ,
+code_four: '<iframe id="vzvd-6870713" name="vzvd-6870713" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6870713/player"></iframe>',
 description:'It’s time to get moving. This beginners cardio program is designed to improve the overall health of your heart, improve cardiovascular endurance, and reduce body fat. Lets start burning those calories',
 style: 'cardiovascular-training',
 schedule: '<div class="table-responsive">
@@ -396,130 +401,7 @@ schedule: '<div class="table-responsive">
     
 ')
 
- Program.create(title: "assessment", level: "level 1", frame: "http://api.vzaar.com/videos/5248691.embed", 
-code_one:'<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player" frameborder="0" height="432" id="vzvd-6517771" mozallowfullscreen name="vzvd-6517771" src="//view.vzaar.com/6517771/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="768"></iframe>',
-schedule: '<div class="table-responsive">
-    
-    <table class="table">
-    
-        <thead>
-    
-            <tr>
-    
-                <th class="text-center">#</th>
-    
-                <th>Sunday</th>
-    
-                <th>Monday</th>
-    
-                <th>Tuesday</th>
-    
-                <th >Wednesday</th>
-    
-                <th >Thursday</th>
-
-                <th >Friday</th>
-
-                <th>Saturday</th>
-    
-            </tr>
-    
-        </thead>
-    
-        <tbody>
-    
-            <tr>
-    
-                <td class="text-center">1</td>                            
-    
-                <td>Flexibility and Mobility</td>
-    
-                <td>Total Body 1</td>
-    
-                <td>Rest Day</td>
-    
-                <td> Core and stability</td>
-
-                <td>Rest Day</td>
-    
-                <td>Total Body 2</td>
-    
-                <td>Rest Day</td>
-    
-    
-                
-    
-            </tr>
-    
-            <tr>
-    
-                
-    
-                <td class="text-center">2</td>
-    
-                <td>Flexibility and Mobility</td>
-    
-                <td>Total Body 1</td>
-    
-                <td>Rest Day</td>
-    
-                <td> Core and stability</td>
-
-                <td>Rest Day</td>
-    
-                <td>Total Body 2</td>
-    
-                <td>Rest Day</td>
-    
-            </tr>
-    
-            <tr>
-    
-                <td class="text-center">3</td>
-    
-                <td>Flexibility and Mobility</td>
-    
-                <td>Total Body 1</td>
-    
-                <td>Rest Day</td>
-    
-                <td> Core and stability</td>
-
-                <td>Rest Day</td>
-    
-                <td>Total Body 2</td>
-    
-                <td>Rest Day</td>
-    
-            </tr>
-    
-            <tr>
-    
-                <td class="text-center">4</td>
-    
-                 <td>Flexibility and Mobility</td>
-    
-                <td>Total Body 1</td>
-    
-                <td>Rest Day</td>
-    
-                <td> Core and stability</td>
-
-                <td>Rest Day</td>
-    
-                <td>Total Body 2</td>
-    
-                <td>Rest Day</td>
-            </tr>
-    
-        </tbody>
-    
-    </table>
-    
-    </div>
-    
-')
-
+ 
  Program.create(title: "General Conditioning 1", level: "Level 2", frame: "http://api.vzaar.com/videos/5248691.embed", 
 code_one: '<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player center-block" frameborder="0" height="432" id="vzvd-5641580" mozallowfullscreen name="vzvd-5641580" src="//view.vzaar.com/5641580/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="768"></iframe>',
 description:'This is a total body program designed to shed those pounds and build lean muscle. This program provides a little taste of everything, resistance training, flexibility & mobility, core conditioning, and cardiovascular training. We are stepping up the intensity but keep up the pace and you will reach your goals',
@@ -647,7 +529,8 @@ schedule: '<div class="table-responsive">
 ')
 
  Program.create(title: "Resistance Training", level: "level 2", frame: "http://api.vzaar.com/videos/5248691.embed", 
-code_one: '<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player center-block" frameborder="0" height="432" id="vzvd-5649198" mozallowfullscreen name="vzvd-5649198" src="//view.vzaar.com/5649198/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="768"></iframe>',
+code_one: '<iframe id="vzvd-6871787" name="vzvd-6871787" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6871787/player"></iframe>',
+code_two: '',
 description:'It’s time to bring up the intensity. Our resistance training program is designed to build muscle, increase muscle endurance, increase strength, and improve mobility. all this just using your own  body weight. No equipment needed!',
 style: 'resistance-training',
 schedule: '<div class="table-responsive">
@@ -772,14 +655,13 @@ schedule: '<div class="table-responsive">
     
 ')
 
- Program.create(title: "tomatoes", level: "level 1", frame: "http://api.vzaar.com/videos/5248691.embed", 
-code_one: '<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player" frameborder="0" height="432" id="vzvd-6517771" mozallowfullscreen name="vzvd-6517771" src="//view.vzaar.com/6517771/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="768"></iframe>',
-style:'background-image: url("https://s3.amazonaws.com/reelmovement/General.jpg")'
-)
-
+ 
 
 Program.create(title: "General Conditioning 3", level: "level 3", frame: "http://api.vzaar.com/videos/5248691.embed", 
-code_one: '<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player center-block" frameborder="0" height="432" id="vzvd-5640112" mozallowfullscreen name="vzvd-5640112" src="//view.vzaar.com/5640112/player" title="vzaar video player" type="text/html" webkitAllowFullScreen width="768"></iframe>',
+code_one: '<iframe id="vzvd-6871494" name="vzvd-6871494" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6871494/player"></iframe>',
+code_two: '',
+code_three: '<iframe id="vzvd-6871517" name="vzvd-6871517" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6871517/player"></iframe>',
+code_four: '<iframe id="vzvd-6871481" name="vzvd-6871481" title="vzaar video player" class="vzaar-video-player center-block" type="text/html" width="768" height="432" frameborder="0" allowFullScreen allowTransparency="true" mozallowfullscreen webkitAllowFullScreen src="//view.vzaar.com/6871481/player"></iframe>',
 description:'Now you are in major leagues. You’re not a novice anymore. This is Level 3 General conditioning. it includes 5 weeks of high intensity exercise that will push you to your limits.Are you ready to step up your game!',
 style: 'general-conditioning-3',
 schedule: '<div class="table-responsive">
