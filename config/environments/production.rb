93d72d354cb0c2bb7cf9fffc6168b00153fffd50
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-04 13:47:39 -0400

    fixed stripe mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,16 +25,21 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+host = 'reelmovement.herokuapp.com'
+config.action_mailer.default_url_options = { host: host }
+ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
-
+:user_name
+:password
+:domain
+:enable_starttls_auto => true
+=> ENV['SENDGRID_USERNAME'],
+=> ENV['SENDGRID_PASSWORD'],
+=> 'heroku.com',
 
 
   # Compress JavaScripts and CSS.
