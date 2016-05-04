4a29562a522af4a9a22b05af286714b9b2245227
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-04 13:53:12 -0400

    fixed stripe mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,14 +33,12 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name
-  :password
-  :domain
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
-=> ENV['SENDGRID_USERNAME'],
-=> ENV['SENDGRID_PASSWORD'],
-=> 'heroku.com',
 }
+
 
 
   # Compress JavaScripts and CSS.
