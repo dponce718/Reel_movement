c16df654da29aac6d66e1a31462d10622acec48b
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-04 14:14:06 -0400

    fixed stripe mailer

--- app/mailers/stripe_mailer.rb
+++ app/mailers/stripe_mailer.rb
@@ -1,12 +1,12 @@
 class StripeMailer < ActionMailer::Base
 
   def new_member(current_user)
-    current_user.name
+    current_user.email
     mail(to: 'daniel.ponce@reelmovement.com', subject: 'Woo! Charge Succeeded!')
   end
 
   def welcome_email(current_user)
-    current_user.name
+    current_user.email
     mail(to: current_user.email, subject: "Thanks for purchasing")
   end
 end
