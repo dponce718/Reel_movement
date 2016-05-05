ca829623442292f94dfaea1d19047b88489cc954
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-05 13:14:38 -0400

    fixed stripe mailer

--- app/mailers/stripe_mailer.rb
+++ app/mailers/stripe_mailer.rb
@@ -1,23 +1,13 @@
 class StripeMailer < ActionMailer::Base
 
-  #def new_member(user)
-#   @user = user
-#    mail(to: 'daniel.ponce@reelmovement.com', subject: 'Woo! Charge Succeeded!')
-#  end#
+  def new_member(customer)
+   @customer = customer
+    mail(to: 'daniel.ponce@reelmovement.com', subject: 'Woo! Charge Succeeded!')
+  end
 
-#  def welcome_email(user)
-#    @user = user
-#    mail to: user.email, subject: "Account activation"
-#  end
-#end
-
-
-
- def admin_dispute_created(charge)
-    @charge = charge
-    @sale = Sale.find_by(stripe_id: @charge.id)
-    if @sale
-      mail(to: 'you@example.com', subject: "Dispute created on charge #{@sale.guid} (#{charge.id})").deliver
-    end
+  def welcome_email(customer)
+    @customer = customer
+    @user = User.find_by!(stripe_id: @customer.id)
+    mail(to: @user.email, subject: "Thanks for purchasing")
   end
 end
