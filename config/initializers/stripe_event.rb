b9e3a02b9eebd128da32b3d98bd6d913f5f5bf7a
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-07 10:28:45 -0400

    added more emails

--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -30,7 +30,7 @@ end
 
  events.subscribe 'customer.deleted' do |event|
   	customer = event.data.object
-    UserMailer.user_deleted(@user).deliver
+    StripeMailer.user_deleted(@user).deliver
 end
 
  events.subscribe 'charge.dispute.created' do |event|
