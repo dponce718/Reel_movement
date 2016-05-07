5454b90ec50c10b21292fd11adcdc9d993c71d01
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-06 21:37:29 -0400

    added more emails

--- app/mailers/stripe_mailer.rb
+++ app/mailers/stripe_mailer.rb
@@ -16,5 +16,42 @@ class StripeMailer < ActionMailer::Base
      @user = user
     mail(to: @user.email, subject: 'failed payment')
   end
+
+  def updated_info(user)
+  	@user = user
+    mail(to: @user.email, subject: 'Thanks for updating your infomation')
+  end
+
+  def user_deleted(user)
+
+  	@user = user
+    mail(to: @user.email, subject: 'failed payment')
+  	
+  end
+
+  def dispute(user)
+  	@user = user
+    mail(to: 'daniel.ponce@reelmovement.com', subject: 'oh no we are having some problems')
+  end
+
+  def dispute_updated(user)
+  	@user = user
+    mail(to: 'daniel.ponce@reelmovement.com', subject: 'dispute updated')
+  end
+
+  def funds_reinstated(user)
+  	@user = user
+    mail(to: 'daniel.ponce@reelmovement.com', subject: 'Woo! you got your money back')
+  end
+
+  def funds_withdrawn(user)
+  	@user = user
+    mail(to: 'daniel.ponce@reelmovement.com', subject: ':( looks like we lost this one')
+  end
+
+  def dispute_closed(user)
+  	@user = user
+    mail(to: 'daniel.ponce@reelmovement.com', subject: 'dispute is closed brother')
+  end
 end
 
--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -10,6 +10,56 @@ StripeEvent.configure do |events|
      customer = event.data.object
     StripeMailer.new_member(customer).deliver
     StripeMailer.welcome_email(customer).deliver
-    StripeMailer.failed_charge(customer).deliver
   end
+
+  events.subscribe 'charge.failed' do |event|
+  	customer = event.data.object
+    StripeMailer.failed_charge(customer).deliver
 end
+
+
+ events.subscribe 'customer.updated' do |event|
+  	customer = event.data.object
+  	StripeMailer.updated_info(customer).deliver
+   
+end
+
+ events.subscribe 'customer.subscription.deleted' do |event|
+  	customer = event.data.object
+    StripeMailer.user_deleted(customer).deliver
+end
+
+ events.subscribe 'charge.dispute.created' do |event|
+  	customer = event.data.object
+  	StripeMailer.dispute(customer).deliver
+    
+end
+
+events.subscribe 'charge.dispute.updated' do |event|
+  	customer = event.data.object
+  	StripeMailer.dispute_updated(customer).deliver
+   
+end
+
+
+events.subscribe 'charge.dispute.funds_reinstated' do |event|
+  	customer = event.data.object
+  	StripeMailer.funds_reinstated(customer).deliver
+    
+end
+
+
+events.subscribe 'charge.dispute.funds_withdrawn' do |event|
+  	customer = event.data.object
+  	StripeMailer.funds_withdrawn(customer).deliver
+   
+end
+
+
+events.subscribe 'charge.dispute.closed' do |event|
+  	customer = event.data.object
+  	StripeMailer.dispute_closed(customer).deliver
+  
+end
+
+
