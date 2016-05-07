b5a55e4adaa5dfcbc6d6061725b0411b6a8a1e9b
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-06 21:46:45 -0400

    added more emails

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -1,39 +1,37 @@
 class SubscribeController < ApplicationController
-#protect_from_forgery :except => :webhook#
+protect_from_forgery :except => :webhook
+  # Method responsbile for handling stripe webhooks
+# reference https://stripe.com/docs/webhooks
+def webhook
 
-#  # Method responsbile for handling stripe webhooks
-## reference https://stripe.com/docs/webhooks
-#def webhook#
-#
-
-#  begin
-#    event_json = JSON.parse(request.body.read)
-#    event_object = event_json['data']['object']
-#    #refer event types here https://stripe.com/docs/api#event_types
-#    case event_json['type']
-#      when 'invoice.payment_succeeded'
-#        
-#      when 'invoice.payment_failed' #credit card does not go through
-#       
-#      when 'charge.failed'
-#        
-#      when 'charge.succeeded'
-#        
-#        when 'customer.created'
-#          StripeMailer.new_member(@user).deliver_now
-#          StripeMailer.welcome_email(@user).deliver_now
-#          StripeMailer.failed_charge(@user).deliver_now
-#        
-#      when 'customer.subscription.deleted'
-#      when 'customer.subscription.updated'
-#    end
-#  rescue Exception => ex
-#    render :json => {:status => 422, :error => "Webhook call failed"}
-#    return
-#  end
-#  render :json => {:status => 200}
-#end
-# 
+  begin
+    event_json = JSON.parse(request.body.read)
+    event_object = event_json['data']['object']
+    #refer event types here https://stripe.com/docs/api#event_types
+    case event_json['type']
+      when 'invoice.payment_succeeded'
+        
+      when 'invoice.payment_failed' #credit card does not go through
+       
+      when 'charge.failed'
+        
+      when 'charge.succeeded'
+        
+        when 'customer.created'
+          StripeMailer.new_member(@user).deliver_now
+          StripeMailer.welcome_email(@user).deliver_now
+          StripeMailer.failed_charge(@user).deliver_now
+        
+      when 'customer.subscription.deleted'
+      when 'customer.subscription.updated'
+    end
+  rescue Exception => ex
+    render :json => {:status => 422, :error => "Webhook call failed"}
+    return
+  end
+  render :json => {:status => 200}
+end
+ 
   def update
     #gets the credit card details submitted in the form
     token = params[:stripeToken]
