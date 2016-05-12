d6dd68f16d946f745226bae1cd6af75a0c43c8e4
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-12 12:08:47 -0400

    appended >> program to users in subscription controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ def update
 
 
 
- current_user.program = Program.find_by_title(params[:user][:programs][:title])
+ program = Program.find_by_title(params[:user][:programs][:title])
 
+ program.users << current_user
  	if current_user.program.save!
   		redirect_to "/subscriptions/show"
   	else
