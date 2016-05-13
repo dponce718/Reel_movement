69aac559e11a1179bf01f3c12e14ebe597503702
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-13 18:28:29 -0400

    fixed text on certain pafes and added click on price on home page

--- app/assets/javascripts/programvideos.js
+++ app/assets/javascripts/programvideos.js
@@ -3,7 +3,9 @@
     
       $('.open-popup-link').magnificPopup({
   type:'inline',
-  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  closeOnBgClick: true,
+  showCloseBtn: false
 });
 
       
