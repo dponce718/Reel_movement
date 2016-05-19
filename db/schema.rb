ae467718bddc17c05aad21b63605ed572c4e5e20
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-19 19:18:49 -0400

    fix nav bar and added calories to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512144000) do
+ActiveRecord::Schema.define(version: 20160519215815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 20160512144000) do
     t.string   "title"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "calories"
   end
 
   create_table "stripe_webhooks", force: :cascade do |t|
