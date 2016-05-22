188f888857b894dcbb6c246cc9b566e3b24ebdfb
Daniel Ponce <d_ponce718@yahoo.com>
2016-05-22 14:51:27 -0400

    ADDED new migration to program

--- app/admin/program.rb
+++ app/admin/program.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Program do
 	permit_params :user_id, :title, :level, :code_one,
     :code_two, :code_three, :code_four, :code_five,
-	 :frame, :schedule, :style,	:start_time, :description		
+	 :frame, :schedule, :style,	:start_time, :description, :title_one		
 
 index do
 
