6f4a38b455d0da0f9d3f1ca03c254bd5e2657981
Daniel Ponce <d_ponce718@yahoo.com>
2016-06-01 09:54:49 -0400

    remove user

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -39,8 +39,8 @@ StripeEvent.configure do |events|
     UserMailer.dispute_closed(event.data.object).deliver
   end
   events.subscribe 'customer.subscription.deleted' do |event|
-    customer = Stripe::Customer.retrieve(current_user.stripeid).delete
-    current_user.destroy
+    customer = Stripe::Customer.retrieve(stripeid: customer.id)
+    User.find_by_stripeid(stripeid: customer.id).destroy
   end
 end
 
