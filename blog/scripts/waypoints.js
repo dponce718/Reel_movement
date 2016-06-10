691cbf3d84454e5f9d82927a89624da9f5a67d2c
Daniel Ponce <d_ponce718@yahoo.com>
2016-06-10 18:24:40 -0400

    added call to action on blog with jquery and waypoints

--- blog/scripts/waypoints.js
+++ blog/scripts/waypoints.js
@@ -4,11 +4,11 @@ var $offer  = $('.remove');
 
 $offer.waypoint(function (direction) {
 	if (direction == 'down') {
-	$('.waypoint-nav').fadeIn(1000, function() {
+	$('.waypoint-nav').fadeIn(800, function() {
        $( ".waypoint-nav" ).removeClass( "hidden" );
     });
 } else {
-	$('.waypoint-nav').fadeOut(1000, function() {
+	$('.waypoint-nav').fadeOut(800, function() {
        $( ".waypoint-nav" ).addClass( "hidden" );
     });
 }
